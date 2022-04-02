7cfc52c8a3dfdda10848f1a67a8573eb07db6633
Frieda Kenned <fkenned@at-p9.msk.21-school.ru>
2022-04-02 20:18:56 +0300

    main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "stack.hpp"
-// #include "vector.hpp"
+// #include "stack.hpp"
+#include "Vector.hpp"
 // #include "stdio.h"
 #include "Stack_test.hpp"
 #include <stack>
 #include <vector>
 #include <deque>
+#include <iostream>
 #define BUFFER_SIZE 4096
 struct Buffer
 {
@@ -16,9 +17,19 @@ int main()
 {
     // ft::Stack<int> st;
 
-    test_Stack<std::stack<int, std::vector<int> >, std::vector<int> >();
+    // test_Stack<std::stack<int, std::vector<int> >, std::vector<int> >();
+
+    ft::Vector<int> vec;
 
 
+    // vec.push_back(5);
+    // vec.pop_back();
+    vec.assign(4,1);
+    std::cout << "size: " << vec.size() << " capacity: " << vec.capacity() << std::endl;
+
+
+    vec.resize(10);
+    std::cout << "size: " << vec.size() << " capacity: " << vec.capacity() << std::endl;
     // ft::vector<int> foo (3,100);   // three ints with a value of 100
     // ft::vector<int> bar (2,200);   // two ints with a value of 200
 
