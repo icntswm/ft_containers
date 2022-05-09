2bcba2f3a0344b7ec2bc51c0c97b3988ebcaa875
Frieda Kenned <fkenned@oa-j5.msk.21-school.ru>
2022-05-09 19:29:22 +0300

    all working

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -1,35 +1,6 @@
 #include "../vector.hpp"
 #include <vector>
 
-class B {
-public:
-	char *l;
-	int i;
-	B():l(nullptr), i(1) {};
-	B(const int &ex) {
-		this->i = ex;
-		this->l = new char('a');
-	};
-	virtual ~B() {
-		delete this->l;
-		this->l = nullptr;
-	};
-};
-
-class A : public B {
-public:
-	A():B(){};
-	A(const B* ex){
-		this->l = new char(*(ex->l));
-		this->i = ex->i;
-		if (ex->i == -1) throw "n";
-	}
-	~A() {
-		delete this->l;
-		this->l = nullptr;
-	};
-};
-
 void insert_test() {
     std::cout << "\033[31m\n --------- iterator insert(iterator position, const_reference val)\033[0m\n";
     ft::vector<int> vec1;
